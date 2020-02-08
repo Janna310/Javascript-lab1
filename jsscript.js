efc53440faf4542a0582c1dcf90630e9e0fbb7c1
Janna <JLodwick310@gmail.com>
2020-02-08 11:11:38 -0500

    Javascript Lab 1

---
+++ jsscript.js
@@ -0,0 +1,36 @@
+/*let names = [];
+
+const name1 = prompt("please enter name one")
+const name2 = prompt("please enter name two")
+const name3 = prompt("please enter name three")
+
+ names.push(name1,name2,name3);
+
+function longestName(names) {
+  return names.reduce((a, b) => a.length < b.length ? b : a, "");
+}
+
+console.log(`The entered names are: ${name1}, ${name2}, ${name3}`);
+console.log(`The longest name is: ${longestName(names)}`);*/
+
+
+//console.log(longestName(names)); 
+
+///////////  OR   //////////////
+
+let names = [];
+
+const name1 = prompt("please enter name one")
+const name2 = prompt("please enter name two")
+const name3 = prompt("please enter name three")
+
+ names.push(name1,name2,name3);
+
+function longest_string(str_ara) {
+  let max = str_ara[0].length;
+  str_ara.map(v => max = Math.max(max, v.length));
+  result = str_ara.filter(v => v.length == max);
+  return result;
+}
+
+console.log(longest_string([name1, name2,name3]))
